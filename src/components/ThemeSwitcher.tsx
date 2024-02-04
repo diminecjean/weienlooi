b1ead7d482d623913d21606c8a2691ba1b68fc9f
Looi Wei En <looi.weien02@student.usm.my>
2024-02-04 22:21:32 +0800

    add ThemeSwitcher component to switch light/dark mode

---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+const ThemeSwitcher = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    setDarkMode(isDarkMode);
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', darkMode);
+    localStorage.setItem('darkMode', darkMode.toString());
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
+  return (
+    <button
+      onClick={toggleDarkMode}
+      // className={`px-4 py-2 rounded-full ${
+      //   darkMode ? 'bg-yellow-400' : 'bg-gray-800'
+      // } ${
+      //   darkMode ? 'text-gray-900' : 'text-white'
+      // } transition-colors duration-200`}
+    >
+      {darkMode ? 
+        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+          <path fill-rule="evenodd" d="M11.7 2a10 10 0 1 0 9.8 13.3 1 1 0 0 0-1-1.3H20a8 8 0 0 1-7.6-10.6l.1-.4a1 1 0 0 0-.8-1Z" clip-rule="evenodd"/>
+        </svg>:
+        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5V3m0 18v-2M7 7 5.7 5.7m12.8 12.8L17 17M5 12H3m18 0h-2M7 17l-1.4 1.4M18.4 5.6 17 7.1M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"/>
+        </svg> 
+  }
+    </button>
+  );
+};
+
+export default ThemeSwitcher;
