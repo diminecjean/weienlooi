05e6e2fb012c3ed8dd631fe6e9f5e96bd9e05f35
Looi Wei En <looi.weien02@student.usm.my>
2024-04-13 16:28:57 +0800

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import './App.css';
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-
-
+import Home from "./pages/Home";
+import Links from "./pages/Links";
 
 const App = () => {
-
   return (
     <div className="App">
       <Routes>
         <Route path="/weienlooi" element={<Home />} />
+        <Route path="/" element={<Links />} />
       </Routes>
     </div>
-  )
+  );
 };
 
 export default App;
