c73abd49848cfd5647059d6eb9b358d8560f6ef8
Looi Wei En <looi.weien02@student.usm.my>
2024-04-22 00:26:21 +0800

    update data

--- src/providers/OrgData.ts
+++ src/providers/OrgData.ts
@@ -2,7 +2,7 @@ export const orgData = [
   {
     orgName: "MoneyLion",
     orgImgSrc:
-      "https://play-lh.googleusercontent.com/ppj2HTbcx_wGpaxTV0iSrZ2eP36tn0QOEqWC4mXDKngroTiVZGT1cJjQXc1qnS7WEaM=w600-h300-pc0xffffff-pd",
+      "https://media.licdn.com/dms/image/D4E0BAQEyNUZXe4pvcw/company-logo_200_200/0/1706651977047/moneylion_logo?e=2147483647&v=beta&t=m3yCr-PzOiBpmyey6Y13To4Nt2GyNcGY8a1TwAX_COQ",
     type: "Profile",
   },
   {
