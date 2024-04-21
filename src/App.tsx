6bcd9c71d5d6c055eca8f7e1b95248aa1278f5a0
Looi Wei En <looi.weien02@student.usm.my>
2024-04-22 00:26:40 +0800

    add gallery route

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Links from "./pages/Links";
+import Gallery from "./pages/Gallery";
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
         <Route path="/weienlooi" element={<Home />} />
+        <Route path="/weienlooi/gallery" element={<Gallery />} />
         <Route path="/weienlooi/links" element={<Links />} />
       </Routes>
     </div>
